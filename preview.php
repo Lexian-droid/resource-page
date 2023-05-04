ed94651a014de91efa14e817b3fd69cf4e0fd7d4
Lexi <83799577+Lexian-droid@users.noreply.github.com>
2023-05-04 01:24:42 -0400

    Update preview.php
    Many "Oopsies" on my part.

--- preview.php
+++ preview.php
@@ -15,6 +15,9 @@
     }
     img {
       max-width: 100%;
+	}
+	video {
+		max-width: 100%;
     }
     .download-button, .back-button {
       display: block;
@@ -31,22 +34,24 @@
   <h1>Preview</h1>
   <a class="back-button" href="/">Back</a>
   <div class="preview-container">
-    <?php
-      if(isset($_GET['file'])) {
-        $file = $_GET['file'];
-        $ext = pathinfo($file, PATHINFO_EXTENSION);
-        if($ext === 'txt') {
-          echo '<pre>' . file_get_contents($file) . '</pre>';
-        } elseif(in_array($ext, ['jpg', 'jpeg', 'png', 'gif'])) {
-          echo '<img src="' . $file . '">';
-        } else {
-          echo 'Unsupported file format';
-        }
-        echo '<a class="download-button" href="' . $file . '" download>Download</a>';
-      } else {
-        echo 'No file specified';
-      }
-    ?>
+	<?php
+	  if(isset($_GET['file'])) {
+		$file = $_GET['file'];
+		$ext = pathinfo($file, PATHINFO_EXTENSION);
+		if(in_array($ext, ['txt', 'doc', 'docx', 'pdf'])) {
+		  echo '<pre>' . file_get_contents($file) . '</pre>';
+		} elseif(in_array($ext, ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff'])) {
+		  echo '<img src="' . $file . '">';
+		} elseif(in_array($ext, ['mp4', 'webm', 'ogg'])) {
+		  echo '<video controls><source src="' . $file . '" type="video/' . $ext . '">Your browser does not support the video tag.</video>';
+		} else {
+		  echo 'Unsupported file format';
+		}
+		echo '<a class="download-button" href="' . $file . '" download>Download</a>';
+	  } else {
+		echo 'No file specified';
+	  }
+	?>
   </div>
 </body>
 </html>
