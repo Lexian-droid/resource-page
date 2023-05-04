ef3b94a8b347e0dd655c4d8dd0d5d4c66de7f3f1
Lexi <83799577+Lexian-droid@users.noreply.github.com>
2023-05-04 01:16:14 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
   define('TITLE', 'My Files');
-  define('BASE_DIR', './files');
+  define('BASE_DIR', './files/');
   define('HOSTNAME', implode('.', array_slice(explode('.', $_SERVER['HTTP_HOST']), -2)));
 ?>
